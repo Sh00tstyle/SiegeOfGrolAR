47885f68ae016d29111c6e7bb28042bebbb88a8f
NICO-LAPTOP\Nico <nico.mazek@googlemail.com>
2019-10-30 14:20:27 +0100

    Adds Singleton base class and an example scene on how to use it for managers

---
+++ Siege of Grol AR/Assets/Scripts/Behaviours/ExampleBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleBehaviour : MonoBehaviour
+{
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Example of using the Singleton of the ExampleManager (accessing it from everywhere)
+            string exampleString = ExampleManager.Instance.GetExampleString();
+            Debug.Log(exampleString);
+        }
+    }
+}
---
+++ Siege of Grol AR/Assets/Scripts/Managers/ExampleManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleManager : Singleton<ExampleManager>
+{
+    protected override void Initialize()
+    {
+        //Use this for initialization
+        Debug.Log("Hello from the ExampleManager");
+    }
+
+    public string GetExampleString()
+    {
+        return "Kappa123";
+    }
+}
