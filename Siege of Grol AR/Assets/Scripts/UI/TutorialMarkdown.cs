6e3402ea9c6b66064ba56b10e17ca88f3cacdf35
Daan <daankemps@hotmail.com>
2019-11-28 23:52:19 +0100

    Changes to TutorialMarkdown and modified a lot of menus to also fit the fonts and sizes

--- Siege of Grol AR/Assets/Scripts/UI/TutorialMarkdown.cs
+++ Siege of Grol AR/Assets/Scripts/UI/TutorialMarkdown.cs
@@ -2,55 +2,164 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialMarkdown : MonoBehaviour
 {
-    [SerializeField] private float _scale;
-    [SerializeField] private Camera _camera;
-    [SerializeField] private GameObject _gameObject;
-    [SerializeField] private RectTransform _highlightCircle, _canvas;
+    [Header("Canvas")]
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private RectTransform _canvas;
 
+    [Header("Text")]
+    [SerializeField] private Text _hintText;
+    [SerializeField] float _textOffset = 20f;
+    [SerializeField] RectTransform _textRect;
+    [SerializeField] float _fadeInTextDuration, _fadeOutTextDuration;
+    [SerializeField] Ease _fadeInTextEase, _fadeOutTextEase;
+
+    [Header("Circle")]
+    [SerializeField] private RectTransform _highlightCircle;
+    [SerializeField] private Button _clickRegion;
+    [SerializeField] private Vector2 _punchScale;
+    [SerializeField] private float _punchDuration = 1.2f;
+    [SerializeField] private Ease _fadeInEase = Ease.InSine, _fadeOutEase = Ease.InSine, _moveInEase = Ease.InSine, _moveOutEase = Ease.InSine;
+    [SerializeField] private float _fadeInDuration = 1, _fadeOutDuration = 2, _moveInDuration = 6, _moveOutDuration;
+
+
+
+
+    // Replace later
+    [SerializeField] GameObject _gameObject;
+
+    private Tween _activeTween;
 
     private Vector2 _offset;
-    private Vector2 _scaleVector;
+    private float _defaultScale;
+    private bool _tutorialRunning;
 
     void Awake()
     {
-        _scaleVector = new Vector2(_scale, _scale); 
-        _offset = new Vector2((float)_canvas.sizeDelta.x / 2f, (float)_canvas.sizeDelta.y / 2f);
+        _defaultScale = _highlightCircle.localScale.x;
+    }
+
+    public void ReplayLastTutorial()
+    {
+        if (!_tutorialRunning)
+            UpdatePosition(_gameObject.transform.position);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            UpdatePosition(_gameObject.transform.position, 1, 12);
+            UpdatePosition(_gameObject.transform.position);
     }
+
 
     private Vector2 GetCanvasPosition(Vector3 pWorldPosition)
     {
-        Vector2 viewportPosition = _camera.WorldToViewportPoint(pWorldPosition);
-        return new Vector2(viewportPosition.x * _canvas.sizeDelta.x, viewportPosition.y * _canvas.sizeDelta.y);
+        Vector2 viewportPosition = CameraManager.Instance.MainCamera.WorldToViewportPoint(pWorldPosition);
+        return new Vector2(
+            ((viewportPosition.x * _canvas.sizeDelta.x) - (_canvas.sizeDelta.x * 0.5f)),
+            ((viewportPosition.y * _canvas.sizeDelta.y) - (_canvas.sizeDelta.y * 0.5f)));
     }
 
-    public void UpdatePosition(Vector3 pWorldPosition, float pScale, float pDuration)
+    public void HighlightClicked()
     {
-        Vector2 canvasPosition = GetCanvasPosition(pWorldPosition) - _offset;
-
-        // Scale the circle
-        _highlightCircle.DOScale(pScale, pDuration);
-        _highlightCircle.DOAnchorPos(canvasPosition, pDuration);
-        //_circle.DOAnchorPos(canvasPosition, 1).OnComplete(() =>
-        //{
-        //    _highlightRect.DOPunchScale(_scaleVector, 1, 1, 1).SetLoops(-1);
-        //    _circle.DOPunchScale(_scaleVector, 1, 1, 1).SetLoops(-1);
-        //});
-        //_highlightRect.DOAnchorPos(canvasPosition, 1);
+        ResetCircle();
     }
 
 
-    private void PunchScaleHighlight()
+    public void UpdatePosition(Vector3 pWorldPosition, bool pBounce = true, bool pBounceInfinite = true, int pBounceLoops = 0, float pPunchDelay = 2)
     {
+        // Wait for Camera to move and focus on object
+        // CameraManager.Instance.Focus();
 
+        //Translate Vector3 WorldPosition to Vector2 UI Space
+        Vector2 CanvasPosition = GetCanvasPosition(pWorldPosition);
+
+        // Enable gameObjects
+        _highlightCircle.gameObject.SetActive(true);
+        _textRect.gameObject.SetActive(true);
+
+        _tutorialRunning = true;
+
+        // Fade in the canvas, if not faded in yet
+        if (_canvasGroup.alpha < 1)
+            _activeTween = FadeCircle(true);
+
+        // If text is not transparent yet, fade it out first
+        if (_hintText.color.a > 0)
+            _activeTween = FadeText(false, Vector2.zero, null);
+
+        _activeTween = FocusCircle(true, CanvasPosition, 1).OnComplete(() =>
+        {
+            //Move Text to location and add text
+            _activeTween = FadeText(true, CanvasPosition, "Click here you piece of shit");
+            // Bounce if neccesary
+            if (pBounce) _activeTween = Punch(_highlightCircle, pBounceInfinite, pBounceLoops).SetDelay(pPunchDelay);
+
+            //Enable user input
+            _clickRegion.interactable = true;
+        });
+    }
+
+
+    private Tween FadeText(bool pFadeIn, Vector2 position, string pText = null)
+    {
+        if (pText != null)
+            _textRect.localPosition = new Vector2(position.x, position.y + _highlightCircle.sizeDelta.y / 2 + _textOffset);
+
+        if (pFadeIn && pText != null)
+            _hintText.text = pText;
+
+        return _hintText.DOFade(
+            pFadeIn ? 1 : 0,
+            pFadeIn ? _fadeInTextDuration : _fadeOutTextDuration)
+        .SetEase(pFadeIn ? _fadeInTextEase : _fadeOutTextEase);
+
+    }
+
+    private Sequence FocusCircle(bool pFocusIn, Vector2 pCanvasPosition, float pScale)
+    {
+        Sequence focusSequence = DOTween.Sequence();
+        focusSequence.Insert(0, _highlightCircle.DOAnchorPos(pCanvasPosition, pFocusIn ? _moveInDuration : _moveOutDuration)
+            .SetEase(pFocusIn ? _moveInEase : _moveOutEase));
+        focusSequence.Insert(0, _highlightCircle.DOScale(pScale, pFocusIn ? _moveInDuration : _moveOutDuration)
+            .SetEase(pFocusIn ? _moveInEase : _moveOutEase));
+        return focusSequence;
+    }
+
+    private void ResetCircle()
+    {
+        _activeTween.Kill();
+
+        // Disable user input
+        _clickRegion.interactable = false;
+
+        FadeText(false, Vector3.zero, null);
+        FocusCircle(false, Vector2.zero, _defaultScale).OnComplete(() =>
+        {
+            // Disable gameObjects
+            _highlightCircle.gameObject.SetActive(false);
+            _textRect.gameObject.SetActive(false);
+
+            _tutorialRunning = false;
+        });
+
+
+
+    }
+
+
+    private Tween Punch(RectTransform pRect, bool pBounceInfinite, int pBounceLoops)
+    {
+        return pRect.DOPunchScale(_punchScale, _punchDuration, 0, 0).SetLoops(pBounceInfinite ? -1 : pBounceLoops);
+    }
+
+    private Tween FadeCircle(bool pFadeIn)
+    {
+        return _canvasGroup.DOFade(pFadeIn ? 1 : 0, pFadeIn ? _fadeInDuration : _fadeOutDuration)
+        .SetEase(pFadeIn ? _fadeInEase : _fadeOutEase);
     }
 
 }
