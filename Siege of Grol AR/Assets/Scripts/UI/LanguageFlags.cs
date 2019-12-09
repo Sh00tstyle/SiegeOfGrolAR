9eb90c4d1811eafcb47b85711f959791be2c732c
DaanKemps <daankemps@hotmail.com>
2019-12-09 10:31:19 +0100

    - Adds the new design assets - Changes the start menu with the new design

--- Siege of Grol AR/Assets/Scripts/UI/LanguageFlags.cs
+++ Siege of Grol AR/Assets/Scripts/UI/LanguageFlags.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class LanguageFlags : MonoBehaviour
 {
-    [SerializeField] Image[] languageFlags;
-    [SerializeField] float flagFadeDuration, minimumOpacity;
+    [SerializeField] Image[] _languageFlags;
+    [SerializeField] float _flagFadeDuration, _minimumOpacity, _maxScale;
 
     Tween[] _activeTweens = new Tween[3];
     int _currentIndex = 4;
@@ -17,8 +17,12 @@ public class LanguageFlags : MonoBehaviour
         if (_currentIndex == index) return;
         _currentIndex = index;
 
-        for (int i = 0; i < languageFlags.Length; ++i)
-            _activeTweens[i] = languageFlags[i].DOFade(i == index ? 1 : minimumOpacity, flagFadeDuration).SetEase(Ease.InOutSine);
+        for (int i = 0; i < _languageFlags.Length; ++i)
+        {
+            _activeTweens[i] = _languageFlags[i].DOFade(i == index ? 1 : _minimumOpacity, _flagFadeDuration).SetEase(Ease.InOutSine);
+            _activeTweens[i] = _languageFlags[i].rectTransform.DOScale(i == index ? _maxScale : 1, _flagFadeDuration).SetEase(Ease.InOutSine);
+        }
+          
 
         // Change settings in GameManager?
         // GameManager.Instance.Save(x);
