3e8ceef17ac5aa576c74912666f1758e672f76ca
Daan <daankemps@hotmail.com>
2020-01-14 14:10:47 +0100

    Adds an AudioMixer, whichs channels can be modified by the options menu.

---
+++ Siege of Grol AR/Assets/Scripts/UI/SoundOptions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundOptions : MonoBehaviour
+{
+
+    [SerializeField] private Slider _ambientSlider, _musicSlider, _sfxSlider;
+    [SerializeField] private AudioMixer _masterMixer;
+
+    private void Awake()
+    {
+        _ambientSlider.value = GetSavedValue(SoundGroup.AMBIENT);
+        _musicSlider.value = GetSavedValue(SoundGroup.MUSIC);
+        _sfxSlider.value = GetSavedValue(SoundGroup.SFX);
+
+        _ambientSlider.onValueChanged.AddListener(delegate { ChangeValue(SoundGroup.AMBIENT, _ambientSlider.value); });
+        _musicSlider.onValueChanged.AddListener(delegate { ChangeValue(SoundGroup.MUSIC, _musicSlider.value); });
+        _sfxSlider.onValueChanged.AddListener(delegate { ChangeValue(SoundGroup.SFX, _sfxSlider.value); });
+    }
+
+    float GetSavedValue(SoundGroup pGroup)
+    {
+        string channel = pGroup.ToString();
+        if (PlayerPrefs.HasKey(channel))
+            return PlayerPrefs.GetFloat(channel);
+        else
+            return 1;
+    }
+
+
+    public void ChangeValue(SoundGroup pGroup, float pValue)
+    {
+        string channel = pGroup.ToString();
+
+        _masterMixer.SetFloat(channel, pValue);
+        PlayerPrefs.SetFloat(channel, pValue);
+    }
+}
---
+++ Siege of Grol AR/Assets/Scripts/Util/SoundGroup.cs
@@ -0,0 +1,6 @@
+﻿public enum SoundGroup
+{
+    MUSIC,
+    AMBIENT,
+    SFX
+}
